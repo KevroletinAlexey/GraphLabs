0a7ab2b2a3b015b9148a4860de69f820be25ab4e
Alexey Kevroletin <kevroletin001@mail.ru>
2022-05-01 13:38:56 +0300

    added get controller

---
+++ GraphLabs.Backend.Api/Controllers/TestControllers/TestsController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using DAL;
+using Domain.Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Results;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Controllers.TestControllers
+{
+    [AllowAnonymous]
+    [Route("[controller]")]
+    //[ApiController]
+    public class TestsController : ODataController
+    {
+        private readonly GraphLabsContext _db;
+
+
+        public TestsController(GraphLabsContext db)
+        {
+            _db = db;
+        }
+        
+        
+        [HttpGet]
+        [EnableQuery]
+        public ActionResult<IQueryable> Get()
+        {
+            var tests = _db.Tests
+                .Select(t => new
+                {
+                    id = t.Id,
+                    subject = t.SubjectId,
+                    name = t.NameTest,
+                    teacherId = t.TeacherId,
+                    questions = t.TestQuestions.Select(q => new
+                    {
+                        id = q.Id,
+                        section = q.SectionId,
+                        text = q.Text,
+                        photo = q.Photo,
+                        difficulty = q.difficulty,
+                        answers = q.TestAnswers.Select(a => new
+                        {
+                            id = a.Id,
+                            text = a.Text
+                        })
+                    })
+                });
+
+            return Ok(tests);
+        }
+
+        
+        // нужны ли veiwModels?
+        public class ViewTest
+        {
+            public long id { get; set; }
+            public string name { get; set; }
+            public virtual ICollection<long> questions { get; set; }
+        }
+        
+    }
+}
